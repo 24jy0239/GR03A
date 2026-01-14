0567616eb369541b599c743072cf0cdbf08ac67e
24jy0241 <24jy0241@C33.jynet.jec.ac.jp>
2026-01-14 12:06:17 +0900

    DAOパッケージ作成しました

---
+++ src/main/java/DAO/addVisitDBAccess.java
@@ -0,0 +1,195 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+public class addVisitDBAccess {
+
+		private Connection createConnection() throws Exception {
+			Connection con = null;
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection(
+						"jdbc:mysql://10.64.144.5:3306/24jy0228",
+						"24jy0228",
+						"24jy0228");
+
+			} catch (Exception e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+				throw new Exception("DB接続処理に失敗しました！管理者に連絡してください。");
+			}
+			return con;
+		}
+
+		private void closeConnection(Connection con) throws Exception {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+				throw new Exception("DB接続処理に失敗しました！管理者に連絡してください。");
+			}
+		}
+
+		public ArrayList<Customer> searchCustomerByTel(String tel) throws Exception {
+			Connection con = createConnection();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			ArrayList<Customer> list = new ArrayList<Customer>();
+			try {
+				if (con != null) {
+					String sql = "SELECT CUSTID, CUSTNAME, KANA, TEL, ADDRESS " +
+							"FROM customer WHERE TEL = ?;";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, tel);
+					rs = pstmt.executeQuery();
+					while (rs.next() == true) {
+						int finid = rs.getInt("CUSTID");
+						String finname = rs.getString("CUSTNAME");
+						String finkana = rs.getString("KANA");
+						String fintel = rs.getString("TEL");
+						String finadd = rs.getString("ADDRESS");
+
+						Customer cust = new Customer(finid, finname, finkana, fintel, finadd);
+						list.add(cust);
+					}
+				}
+			} catch (SQLException e) {
+				System.out.println(
+						"DB切断時にエラーが発生しました（商品検索）。");
+				e.printStackTrace();
+				throw new Exception("顧客情報検索処理に失敗しました！管理者に連絡してください。");
+			} finally {
+				try {
+					if (rs != null) {
+						rs.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+				try {
+					if (pstmt != null) {
+						pstmt.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+			}
+			closeConnection(con);
+			return list;
+		}
+
+		public ArrayList<Customer> searchCustomerByKana(String kana) throws Exception {
+			Connection con = createConnection();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			ArrayList<Customer> list = new ArrayList<Customer>();
+			try {
+				if (con != null) {
+					String sql = "SELECT CUSTID, CUSTNAME, KANA, TEL, ADDRESS " +
+							"FROM customer WHERE KANA LIKE ?;";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, "%" + kana + "%");
+					rs = pstmt.executeQuery();
+					while (rs.next() == true) {
+						int finid = rs.getInt("CUSTID");
+						String finname = rs.getString("CUSTNAME");
+						String finkana = rs.getString("KANA");
+						String fintel = rs.getString("TEL");
+						String finadd = rs.getString("ADDRESS");
+
+						Customer cust = new Customer(finid, finname, finkana, fintel, finadd);
+						list.add(cust);
+					}
+				}
+			} catch (SQLException e) {
+				System.out.println(
+						"DB切断時にエラーが発生しました（商品検索）。");
+				e.printStackTrace();
+				throw new Exception("顧客情報検索処理に失敗しました！管理者に連絡してください。");
+			} finally {
+				try {
+					if (rs != null) {
+						rs.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+				try {
+					if (pstmt != null) {
+						pstmt.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+			}
+			closeConnection(con);
+			return list;
+		}
+
+		public ArrayList<Customer> searchCustomer(String tel, String kana) throws Exception {
+			Connection con = createConnection();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			ArrayList<Customer> list = new ArrayList<Customer>();
+			try {
+				if (con != null) {
+					String sql = "SELECT CUSTID, CUSTNAME, KANA, TEL, ADDRESS " +
+							"FROM customer WHERE TEL = ? AND KANA LIKE ?;";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setString(1, tel);
+					pstmt.setString(2, "%" + kana + "%");
+					rs = pstmt.executeQuery();
+					while (rs.next() == true) {
+						int finid = rs.getInt("CUSTID");
+						String finname = rs.getString("CUSTNAME");
+						String finkana = rs.getString("KANA");
+						String fintel = rs.getString("TEL");
+						String finadd = rs.getString("ADDRESS");
+
+						Customer cust = new Customer(finid, finname, finkana, fintel, finadd);
+						list.add(cust);
+					}
+				}
+			} catch (SQLException e) {
+				System.out.println(
+						"DB切断時にエラーが発生しました（商品検索）。");
+				e.printStackTrace();
+				throw new Exception("顧客情報検索処理に失敗しました！管理者に連絡してください。");
+			} finally {
+				try {
+					if (rs != null) {
+						rs.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+				try {
+					if (pstmt != null) {
+						pstmt.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("DB切断時にエラーが発生しました。");
+					e.printStackTrace();
+				}
+			}
+			closeConnection(con);
+			return list;
+		}
+	}
+
+
